d7fa913ffc6bafe7e46d1b9b1487514550deafa4
homingcloud <scutariunico9@gmail.com>
2026-04-04 21:28:35 -0400

    Update and trying from SSH

--- Ass.03_For-Loops/Personal-Code_V1/assignment2a.py
+++
@@ -1,6 +0,0 @@
-print('Hello world')
-x=0
-while true:
-  x=x+1
-  go=int(input('Do I keep on adding?'))
-  print(x)
---
+++ Ass.03_For-Loops/Personal-Code_V1/assignment3a.py
@@ -0,0 +1,15 @@
+print('Hello world')
+x=0
+print(x)
+while true:
+  x=x+1
+  print(x)
+  go=int(input('Do I keep on adding? Type 1 for yes or 2 for no\n='))
+ if go==1:
+  continue
+ elif go==2:
+  false
+ else:
+  print('Sorry that is an invalid input please reload the program.')
+  exit()
+
